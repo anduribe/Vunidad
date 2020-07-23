e78bc705f2957ef3cdb18b8abee82533b0c36743
anduribe <anduribe@gmail.com>
2020-07-23 19:08:01 -0400

    segunda subida clave encriptada

--- src/main/java/com/vunidad/ConfiguracionSeguridad.java
+++ src/main/java/com/vunidad/ConfiguracionSeguridad.java
@@ -23,12 +23,18 @@ public class ConfiguracionSeguridad extends WebSecurityConfigurerAdapter {
 	@Autowired
 	AuthenticationSuccessHandler successHandler;
 	
+	//vamos a encriptar la clave
+	@Bean
+	public PasswordEncoder PasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
-			.withUser("123").password("{noop}123").roles("USER")
+			.withUser("123").password("$2a$10$5.zbF9YwBjPb0zHJKe7m.e.ZrbgGAS7nCb0ENvDCWeSny21W8oT/.").roles("USER")
 			.and()
-			.withUser("1234").password("{noop}1234").roles("ADMIN");
+			.withUser("1234").password("$2a$10$6GL9S7zSlvHKXEXpRw0rIe4Le9NIG5hZmuhL3zqp5nWGMtU5DkDea").roles("ADMIN");
 	}
 
 	@Override
